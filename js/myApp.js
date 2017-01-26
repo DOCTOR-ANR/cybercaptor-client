15b648c4f3362703c5ef8d6fca5039d04bb3fe15
Théo Combe <theocombe@thalesgroup.com>
2017-01-26 10:25:28 +0100

    fix

--- js/myApp.js
+++ js/myApp.js
@@ -40,7 +40,9 @@ var myApp = angular.module('CyberCAPTOR', ['routeAppControllers', 'ngRoute', 'ng
 myApp.constant("myConfig", {
 	// URL base for REST request
     "path" : "cybercaptor-server/rest/json",
-    "configPath" : "cybercaptor-server/rest/json/configuration/remediation-cost-parameters"
+    "configPath" : "cybercaptor-server/rest/json/configuration/remediation-cost-parameters",
+    "url" : "http://127.0.0.1:8080/cybercaptor-server/rest/json",
+    "config" : "http://127.0.0.1:8080/cybercaptor-server/rest/json/configuration/remediation-cost-parameters"
 });
 
 /**
